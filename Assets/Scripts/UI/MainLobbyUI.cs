b26c53a9bd7460f6cd88d5baa8b7c43622fa9c4b
jaeho <>
2026-08-04 16:48:51 +0900

    [feat] UI 버튼과 메인로비UI 틀 작성

---
+++ Assets/Scripts/UI/MainLobbyUI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class MainLobbyUI : UIBase
+{
+    [SerializeField] private UIButton Button_GameStart;
+    [SerializeField] private UIButton Button_GameQuit;
+    [SerializeField] private UIButton Button_LoadGame;
+
+    private void OnEnable()
+    {
+        
+    }
+
+    public void OnClick_GameStart()
+    {
+
+    }
+
+    public void OnClick_GameQuit()
+    {
+
+    }
+
+    // 로드는 나중에 구현
+    public void OnClick_LoadGame()
+    {
+
+    }
+}
---
+++ Assets/Scripts/UI/UIButton.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIButton : MonoBehaviour
+{
+    [SerializeField] private Button Button_Base;
+    [SerializeField] private Text Text_Base;
+    [SerializeField] private Image Image_Base;
+    [SerializeField] private Image Image_Select;
+
+    // 수동으로 끊어줌
+    private bool _isSlotMenualUnbindEvent;
+
+    private void Awake()
+    {
+        InitUIButton();
+        SetDefaultUI();
+    }
+
+    private void OnDisable()
+    {
+        if (_isSlotMenualUnbindEvent == false)
+        {
+            Button_Base.onClick.RemoveAllListeners();
+        }
+    }
+
+    private void SetDefaultUI()
+    {
+        if (Image_Select != null) 
+        {
+            Image_Select.gameObject.SetActive(false);
+        }
+    }
+
+    private void InitUIButton()
+    {
+        if(Button_Base != null)
+        {
+            return;
+        }
+
+        // 외부에서 등록할 수 있고, 누락했다면 등록안해도 알아서 찾아주도록 로직을 넣어놨다
+        var button = this.gameObject.GetComponentInChildren<Button>();
+        if (button != null)
+        {
+            this.Button_Base = button;
+        }
+    }
+
+    public void BindOnClickButtonEvent(Action onClickCallback, bool isMenualUnbineEvent = false)
+    {
+        if (Button_Base == null)
+        {
+            Button_Base.onClick.AddListener(onClickCallback.Invoke);
+            _isSlotMenualUnbindEvent = isMenualUnbineEvent;
+        }
+    }
+
+    public void UnBindAllOnClickButtonEvent()
+    {
+        if (Button_Base == null) return;
+
+        Button_Base.onClick.RemoveAllListeners();
+    }
+
+    public void ChangeButtonText(string buttonStr)
+    {
+        //혹시 이 버튼을 동적으로 코드에서 텍스트 수정해야할 때 사용
+        if (Text_Base == null) return;
+
+        Text_Base.text = buttonStr;
+    }
+}
