d631aa25991bd82d39084a631d3ebb64f21bbda4
jaeho <>
2026-08-04 12:28:05 +0900

    [feat] GameUtil / NetworkManager / ResourceManager 틀 작성

---
+++ Assets/Scripts/Manager/GameUtil.cs
@@ -0,0 +1,67 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using UnityEditor.Animations;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class GameUtil
+{
+    // 마지막으로 할당된 ID를 전역적으로 기록 (스레드 안전)
+    private static long _lastId = 0;
+
+    public static void LoadFullData()
+    {
+        // 게임 로딩할때 불러올 데이터는 여기서
+        GameDataManager.Instance.LoadAll();
+    }
+
+    public static Sprite LoadSpriteCanBeNull(string spriteName)
+    {
+        // Resources/ 경로에서 이름으로 스프라이트 로드
+        // 예: spriteName이 "Sword"라면 Assets/Resources/2D/Sword.png를 찾음
+        // 이 2D같은 경로는 나중에 Sprite, Texture 등등 다양하게 바꿔도 무관합니다
+        Sprite loadedSprite = Resources.Load<Sprite>($"{spriteName}");
+
+        if (loadedSprite != null)
+        {
+            return loadedSprite;
+        }
+
+        Debug.LogError($"에셋을 찾을 수 없습니다: {spriteName}");
+        return null;
+    }
+
+    public static async UniTask<Sprite> LoadAndSetSpriteImage(Image targetImage, string spritePath)
+    {
+        Sprite sprite = await ResourceManager.Inst.LoadSprite(spritePath);
+        if (sprite != null)
+        {
+            targetImage.sprite = sprite;
+        }
+        return sprite;
+    }
+
+    public static async UniTaskVoid LoadAndPlayAudioClip(AudioSource audioSource, string audioPath, bool isLoop = false)
+    {
+        AudioClip clip = await ResourceManager.Inst.LoadAsset<AudioClip>(audioPath);
+        if (clip == null)
+        {
+            Debug.LogError($"{audioPath}를 찾을 수 없습니다! 어드레서블 설정이 되어 있는지 확인해주세요.");
+            return;
+        }
+
+        if (isLoop == true)
+        {
+            audioSource.clip = clip;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+        else
+        {
+            audioSource.PlayOneShot(clip);
+        }
+    }
+}
---
+++ Assets/Scripts/Manager/NetworkManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class NetworkManager : MonoBehaviour
+{
+    public static NetworkManager Inst { get; set; }
+
+    private void Awake()
+    {
+        Inst = this;
+        InitNetworkService();
+    }
+
+    private void InitNetworkService()
+    {
+        // 앞으로 네트워크 매니저에 사용할 다양한 서비스를 생성
+
+    }
+}
