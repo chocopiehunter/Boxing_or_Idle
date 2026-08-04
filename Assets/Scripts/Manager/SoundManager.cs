e530f8b37cae98533e042b5de2220ccd9fbd9890
jaeho <>
2026-08-04 22:56:09 +0900

    [feat] 사운드 / 세팅UI 작업

---
+++ Assets/Scripts/Manager/SoundManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance { get; set; }
+
+    [SerializeField] private AudioMixer Mixer;
+
+    private const string Mixer_Param_Master_Volume = "MasterVolume";
+
+    private const string Pref_Key_Master_Volume = "MasterVolume";
+
+    private void Awake()
+    {
+        Instance = this;
+        InitVolume();
+    }
+
+    private void InitVolume()
+    {
+        float savedVolume = PlayerPrefs.HasKey(Pref_Key_Master_Volume)
+            ? PlayerPrefs.GetFloat(Pref_Key_Master_Volume) : 1f;
+
+        SetMasterVolume(savedVolume);
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        // 슬라이더값 0~1을 그대로 넣으면 안되고 데시벨값으로 변환해야함
+        float dbVolume = (volume <= 0.0001f) ? -80f : Mathf.Log10(volume) * 20f;
+        Mixer.SetFloat(Mixer_Param_Master_Volume, dbVolume);
+
+        PlayerPrefs.SetFloat(Pref_Key_Master_Volume, volume);
+    }
+
+    public float GetMasterVolume()
+    {
+        return PlayerPrefs.HasKey(Pref_Key_Master_Volume)
+            ? PlayerPrefs.GetFloat(Pref_Key_Master_Volume) : 1f;
+    }
+}
---
+++ Assets/Scripts/UI/SettingUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingUI : MonoBehaviour
+{
+    [SerializeField] private Slider Slider_MasterVolume;
+
+    private void OnEnable()
+    {
+        float currentVolume = SoundManager.Instance.GetMasterVolume();
+        Slider_MasterVolume.SetValueWithoutNotify(currentVolume);
+
+        Slider_MasterVolume.onValueChanged.AddListener(OnMasterVolumeChanged);
+    }
+
+    private void OnDisable()
+    {
+        Slider_MasterVolume.onValueChanged.RemoveListener(OnMasterVolumeChanged);
+    }
+
+    private void OnMasterVolumeChanged(float value)
+    {
+        SoundManager.Instance.SetMasterVolume(value);
+    }
+}
