c0b7a500fb9621b541977b7e6d3a21aa050df357
jaeho <>
2026-08-04 16:47:50 +0900

    [feat] GameManager 틀 작성

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        // LoadSaveData();
+    }
+
+    public void StartGame()
+    {
+
+    }
+
+    public void SaveData()
+    {
+
+    }
+
+    public void SaveAndEndGame()
+    {
+
+    }
+
+    private void LoadSaveData()
+    {
+
+    }
+}
