2c4fd0f27c301e0c7513150a930cdaea86bd87df
jaeho <>
2026-08-04 13:45:26 +0900

    [feat] UIManager 및 UI스크립트 준비

---
+++ Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] Canvas Canvas_BgRoot;
+    [SerializeField] Canvas Canvas_MainRoot;
+    [SerializeField] Canvas Canvas_ContentRoot;
+    [SerializeField] Canvas Canvas_PopupRoot;
+    [SerializeField] Canvas Canvas_VeryFrontRoot;
+
+    public static UIManager Instance { get; set; }
+
+    private Dictionary<UIType, UIBase> _createdUIDic = new Dictionary<UIType, UIBase>();
+
+    private HashSet<UIType> _openedUIDic = new HashSet<UIType>();
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        this.ShowStartupUIOnGameStart();
+    }
+
+    public UIBase OpenUI(UIRootType uiRootType, UIType uiType, bool isInitialHide = false)
+    {
+        // 딱히 요청이 있진 않고 오픈만 하면 되는 UI에서 사용
+        var openedUI = GetCreatedUI(uiRootType, uiType);
+
+        bool isSetActiveOnOpen = (isInitialHide == false);
+        if (_openedUIDic.Contains(uiType) == false)
+        {
+            openedUI.gameObject.SetActive(isSetActiveOnOpen);
+            _openedUIDic.Add(uiType);
+        }
+
+        return openedUI;
+    }
+
+    public void CloseUI(UIRootType uiRootType, UIType uiType)
+    {
+        if (_openedUIDic.Contains(uiType))
+        {
+            var openedUi = _createdUIDic[uiType];
+            openedUi.gameObject.SetActive(false);
+            _openedUIDic.Remove(uiType);
+        }
+    }
+
+    private Transform GetRootTransform(UIRootType uiRootType)
+    {
+        Transform root = null;
+        switch (uiRootType)
+        {
+            case UIRootType.BackgroundUI:
+                root = Canvas_BgRoot.transform;
+                break;
+            case UIRootType.MainUI:
+                root = Canvas_MainRoot.transform;
+                break;
+            case UIRootType.ContentUI:
+                root = Canvas_ContentRoot.transform;
+                break;
+            case UIRootType.PopupUI:
+                root = Canvas_PopupRoot.transform;
+                break;
+            case UIRootType.VeryFrontUI:
+                root = Canvas_VeryFrontRoot.transform;
+                break;
+        }
+
+        return root;
+    }
+
+    private void CreateUI(UIRootType uiRootType, UIType uiType)
+    {
+        if (_createdUIDic.ContainsKey(uiType) == false)
+        {
+            string path = this.GetUIPath(uiRootType, uiType);
+            GameObject loadedObj = (GameObject)Resources.Load(path);
+            Transform root = GetRootTransform(uiRootType);
+            GameObject gObj = Instantiate(loadedObj, root);
+            if(gObj != null)
+            {
+                var uiBase = gObj.GetComponent<UIBase>();
+                _createdUIDic.Add(uiType, uiBase);
+            }
+        }
+    }
+
+    public UIBase GetOpenedUI(UIRootType uiRootType, UIType uiType)
+    {
+        return GetCreatedUI(uiRootType, uiType);
+    }
+
+    private UIBase GetCreatedUI(UIRootType uiRootType, UIType uiType)
+    {
+        if (_createdUIDic.ContainsKey(uiType) == false)
+        {
+            CreateUI(uiRootType, uiType);
+        }
+
+        return _createdUIDic[uiType];
+    }
+
+    public UIBase OpenContentUI(UIType uiType)
+    {
+        return OpenUI(UIRootType.ContentUI, uiType);
+    }
+
+    public UIBase OpenPopupUI(UIType uiType)
+    {
+        return OpenUI(UIRootType.PopupUI, uiType);
+    }
+
+    public void CloseContentUI(UIType uiType)
+    {
+        CloseUI(UIRootType.ContentUI, uiType);
+    }
+
+    public void ClosePopupUI(UIType uiType)
+    {
+        CloseUI(UIRootType.PopupUI, uiType);
+    }
+}
---
+++ Assets/Scripts/Manager/UIManagerExtension.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public enum UIRootType
+{
+    None = 0,
+    BackgroundUI,
+    MainUI,
+    ContentUI,
+    PopupUI,
+    VeryFrontUI
+}
+
+public enum UIType
+{
+    MainUI,
+    LoadingUI,
+    LobbyUI,
+    Inventory,
+    DialogueUI,
+    SimplePopupUI,
+
+}
+
+public static class UIManagerExtension
+{
+    public static string GetUIPath(this UIManager uiManager, UIRootType uiRootType, UIType uiType)
+    {
+        string path = string.Empty;
+
+        path = $"Prefabs/UI/{uiRootType}/{uiType}";
+        return path;
+    }
+
+    public static void ShowStartupUIOnGameStart(this UIManager uiManager)
+    {
+
+    }
+}
