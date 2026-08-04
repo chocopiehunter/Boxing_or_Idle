0156a0c047e847832a4d7e9e62189e3f25418a6d
jaeho <>
2026-08-04 12:21:23 +0900

    [feat] GameData / GameDataManager 틀 작성

---
+++ Assets/Scripts/Manager/GameData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class GameDataBase
+{
+    public string Id;
+}
+
+[System.Serializable]
+public class CharacterData : GameDataBase
+{
+
+}
---
+++ Assets/Scripts/Manager/GameDataManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using UnityEngine;
+
+public class GameDataManager : MonoBehaviour
+{
+    public static GameDataManager Instance { get; set; }
+
+    private void Awake()
+    {
+        Instance = this;
+
+        // GameUtil.LoadFullData();
+    }
+
+    [Serializable]
+    private class SerializationWrapper<T>
+    {
+        public List<T> items;
+    }
+
+    // 1. 등록
+    public Dictionary<string, CharacterData> CharacterDataList { get; private set; } = new Dictionary<string, CharacterData>();
+
+    private Dictionary<string, T> LoadData<T>(string tableName) where T : GameDataBase
+    {
+        // 경로설정
+        // Resources/JsonOutput 폴더
+        string resourcePath = $"JsonOutput/{tableName}";
+
+        // 리소스 로드
+        TextAsset textAsset = Resources.Load<TextAsset>(resourcePath);
+
+        // 파일 존재 여부 체크
+        if (textAsset == null)
+        {
+            Debug.LogError($"[Error] 리소스를 찾을 수 없습니다: Resources/{resourcePath}");
+            return new Dictionary<string, T>();
+        }
+
+        try
+        {
+            string jsonString = textAsset.text;
+
+            // Wrapper 트릭 적용
+            string wrappedJson = "{\"items\":" + jsonString + "}";
+            SerializationWrapper<T> wrapper = JsonUtility.FromJson<SerializationWrapper<T>>(wrappedJson);
+
+            if (wrapper != null && wrapper.items != null)
+            {
+                Debug.Log($"{typeof(T).Name} 데이터를 {wrapper.items.Count}개 로드했습니다.");
+                // ToDictionary를 사용하려면 각 클래스(T)에 Id 필드가 있어야 합니다
+                return wrapper.items.ToDictionary(item => item.Id.ToString());
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"[{typeof(T).Name} JSON 로드 오류] {ex.Message}");
+        }
+
+        return new Dictionary<string, T>();
+    }
+
+    // 3. 로드
+    public void LoadAll()
+    {
+
+    }
+
+    // 2. 사용을 위한 메서드 정의
+    
+    public CharacterData GetCharacterData(string Id)
+    {
+        if (CharacterDataList == null || string.IsNullOrEmpty(Id)) return null;
+
+        return CharacterDataList.TryGetValue(Id, out var item) ? item : null;
+    }
+}
