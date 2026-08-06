3be5ed773bb55ef7df14e6e6ca442d253455d12c
jaeho <>
2026-08-06 18:15:25 +0900

    [feat] SeasonManager 계절과 턴 구현 중간저장

--- Assets/Scripts/Manager/SeasonManager.cs
+++ Assets/Scripts/Manager/SeasonManager.cs
@@ -1,11 +1,35 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class SeasonManager : MonoBehaviour
 {
     public static SeasonManager Instance { get; set; }
 
+    public int Year { get; private set; } = 1;
+    public Season CurrentSeason { get; private set; } = Season.Spring;
+
+    public event Action OnTurnAdvanced;
+
     private void Awake()
     {
         Instance = this;
+    }
+
+    public void AdvanceTurn()
+    {
+        
+    }
+
+    public void AdvanceSeason()
+    {
+        if (CurrentSeason == Season.Winter)
+        {
+            CurrentSeason = Season.Spring;
+            Year++;
+        }
+        else
+        {
+            CurrentSeason++;
+        }
     }
 }
