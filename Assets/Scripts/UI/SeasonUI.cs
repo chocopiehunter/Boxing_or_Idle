ed71b9eebc35f125a3be0ddddad3ce25b264c855
jaeho <>
2026-08-07 17:57:12 +0900

    [feat] SeasonUI 중간저장

---
+++ Assets/Scripts/UI/SeasonUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SeasonUI : UIBase
+{
+    [SerializeField] private Image Image_SeasonIcon;
+    [SerializeField] private Text Text_Week;
+
+    private const string WeekFromat = "{0}주차";
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    private void Refresh()
+    {
+
+    }
+}
