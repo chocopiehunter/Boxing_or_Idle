71e25a05429f1c6f17b11c0dbbe7d74178b91a4e
jaeho <>
2026-08-07 18:10:51 +0900

    [feat] SeasonManager 턴 -> Week 개념 변경 / 일주일 진행 로직

--- Assets/Scripts/Manager/SeasonManager.cs
+++ Assets/Scripts/Manager/SeasonManager.cs
@@ -5,19 +5,31 @@ public class SeasonManager : MonoBehaviour
 {
     public static SeasonManager Instance { get; set; }
 
+    public const int WeeksPerSeason = 4;
+
     public int Year { get; private set; } = 1;
     public Season CurrentSeason { get; private set; } = Season.Spring;
+    public int CurrentWeek { get; private set; } = 1;
 
-    public event Action OnTurnAdvanced;
+
+    public event Action OnWeekAdvanced;
 
     private void Awake()
     {
         Instance = this;
     }
 
-    public void AdvanceTurn()
+    public void AdvanceWeek()
     {
+        CurrentWeek++;
         
+        if(CurrentWeek > WeeksPerSeason)
+        {
+            CurrentWeek = 1;
+            AdvanceSeason();
+        }
+
+        OnWeekAdvanced?.Invoke();
     }
 
     public void AdvanceSeason()
