4ac4a1f594493106cb49eb7fce9aef750d917ed4
jaeho <>
2026-08-07 18:33:56 +0900

    [add] Scriptable Object 폴더 생성 및 SeasonIconTable(SO)생성

--- Assets/Scripts/Data/SeasonIconTable.cs
+++ Assets/Scripts/Data/SeasonIconTable.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "SeasonIconTable", menuName = "GameData/SeasonIconTable")]
@@ -17,9 +17,13 @@ public class SeasonIconTable : ScriptableObject
     {
         foreach (SeasonIconEntry entry in Entries)
         {
-
+            if (entry.Season == season)
+            {
+                return entry.IconAddress;
+            }
         }
 
+        Debug.LogError($"계절 아이콘 주소가 없습니다 {season}");
         return string.Empty;
     }
 }
