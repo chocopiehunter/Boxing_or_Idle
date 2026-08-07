d8ba8e1cca4719b2420ebf5e0550c5435ae01762
jaeho <>
2026-08-07 19:07:41 +0900

    [feat] SeasonUI 계절 아이콘과 n주차 표시 로직

--- Assets/Scripts/UI/SeasonUI.cs
+++ Assets/Scripts/UI/SeasonUI.cs
@@ -1,25 +1,54 @@
-﻿using UnityEngine;
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class SeasonUI : UIBase
 {
-    [SerializeField] private Image Image_SeasonIcon;
+    [SerializeField] private Image Image_Season;
     [SerializeField] private Text Text_Week;
+    [SerializeField] private SeasonIconTable IconTable;
 
-    private const string WeekFromat = "{0}주차";
+    private const string WeekTextFormat = "{0}주차";
 
     private void OnEnable()
     {
-        
+        if (SeasonManager.Instance != null)
+        {
+            SeasonManager.Instance.OnWeekAdvanced += Refresh;
+        }
+
+        Refresh();
     }
 
     private void OnDisable()
     {
-        
+        if (SeasonManager.Instance != null)
+        {
+            SeasonManager.Instance.OnWeekAdvanced -= Refresh;
+        }
     }
 
     private void Refresh()
     {
+        if (SeasonManager.Instance == null)
+        {
+            Debug.LogError("SeasonManager가 없습니다");
+            return;
+        }
 
+        Text_Week.text = string.Format(WeekTextFormat, SeasonManager.Instance.CurrentWeek);
+        SetSeasonIconAsync(SeasonManager.Instance.CurrentSeason).Forget();
+    }
+
+    private async UniTaskVoid SetSeasonIconAsync(Season season)
+    {
+        string address = IconTable.GetIconAddress(season);
+
+        if (string.IsNullOrEmpty(address) == true)
+        {
+            return;
+        }
+
+        await GameUtil.LoadAndSetSpriteImage(Image_Season, address);
     }
 }
