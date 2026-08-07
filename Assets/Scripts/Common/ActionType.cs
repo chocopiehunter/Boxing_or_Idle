e0e6cf7dc9a3556edf9faa3363f427cd87ab50b3
jaeho <>
2026-08-08 01:38:56 +0900

    [feat] TrainingUI 작성 중간저장

--- Assets/Scripts/Common/ActionType.cs
+++ Assets/Scripts/Common/ActionType.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public enum TurnType
+public enum ActionType
 {
     Training,
     Rest
---
+++ Assets/Scripts/UI/TrainingUI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+public class TrainingUI : UIBase
+{
+    [SerializeField] private UIButton Button_Training;
+    [SerializeField] private UIButton Button_Rest;
+
+    public event Action<ActionType> OnActionSelected;
+
+    private void OnClick_Training()
+    {
+
+    }
+
+    private void OnClick_Rest()
+    {
+
+    }
+
+    private void SelectAction(ActionType actionType)
+    {
+
+    }
+}
