9c112614c5768fe0dc2932365f817197a8129feb
jaeho <>
2026-08-07 19:47:28 +0900

    [feat] MainUI의 구조 변경 / SeasonUI 구조 변경 완료

--- Assets/Scripts/UI/MainUI.cs
+++ Assets/Scripts/UI/MainUI.cs
@@ -2,20 +2,30 @@
 
 public class MainUI : UIBase
 {
-    [SerializeField] private SeasonUI SeasonUI;
+    [SerializeField] private SeasonUI UI_Season;
 
     private void OnEnable()
     {
-        
+        if(SeasonManager.Instance == null)
+        {
+            Debug.LogError("SeasonManager가 씬에 없습니다");
+            return;
+        }
+
+        SeasonManager.Instance.OnWeekAdvanced += RefreshSeasonUI;
+        RefreshSeasonUI();
     }
 
     private void OnDisable()
     {
-        
+        if(SeasonManager.Instance != null)
+        {
+            SeasonManager.Instance.OnWeekAdvanced -= RefreshSeasonUI;
+        }
     }
 
     private void RefreshSeasonUI()
     {
-
+        UI_Season.SetSeason(SeasonManager.Instance.CurrentSeason, SeasonManager.Instance.CurrentWeek);
     }
 }
