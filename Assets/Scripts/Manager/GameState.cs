ddc5b400bdf00e3d424b76460370a0a757967788
jaeho <>
2026-08-08 19:49:23 +0900

    [feat] GameState에 배속 상태 추가

--- Assets/Scripts/Manager/GameState.cs
+++ Assets/Scripts/Manager/GameState.cs
@@ -3,9 +3,15 @@
 public class GameState
 {
     public GameFlowState CurrentState { get; private set; } = GameFlowState.Title;
+    public GameSpeedType CurrentSpeed { get; private set; } = GameSpeedType.Normal;
 
     public void ChangeState(GameFlowState newState)
     {
         CurrentState = newState;
+    }
+
+    public void ChangeSpeed(GameSpeedType newSpeed)
+    {
+        CurrentSpeed = newSpeed;
     }
 }
