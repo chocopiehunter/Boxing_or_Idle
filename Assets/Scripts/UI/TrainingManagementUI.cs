365cd19455cec2020b34307a4f0dd77fc8d9548e
jaeho <>
2026-08-11 02:04:31 +0900

    [feat] TrainingManagementUI 훈련 정책 로직 구현

--- Assets/Scripts/UI/TrainingManagementUI.cs
+++ Assets/Scripts/UI/TrainingManagementUI.cs
@@ -22,7 +22,12 @@ public class TrainingManagementUI : UIBase
         Button_AtkTraining.BindOnClickButtonEvent(OnClick_AtkTraining);
         Button_DefTraining.BindOnClickButtonEvent(OnClick_DefTraining);
         Button_Close.BindOnClickButtonEvent(OnClick_Close);
+
+        _targetFighter = FighterManager.Instance.GetFirstFighter();
+        RefreshUI();
     }
+
+    
 
     private void RefreshUI()
     {
@@ -36,25 +41,53 @@ public class TrainingManagementUI : UIBase
 
         Text_Name.text = _targetFighter.Name;
 
+        TrainingData trainingData = GameDataManager.Instance.GetTrainingData(_targetFighter.CurrentTrainingId);
+
+        string trainingName;
+        if (trainingData != null)
+        {
+            trainingName = trainingData.Name;
+        }
+        else
+        {
+            trainingName = _targetFighter.CurrentTrainingId;
+        }
+
+        Text_CurrentTraining.text = string.Format("현재 훈련 정책: {0}", trainingName);
+
+        Text_Stats.text = $"Hp {_targetFighter.Hp} / Atk {_targetFighter.Atk} / Def {_targetFighter.Def} / Condition {_targetFighter.Condition}";
+    }
+
+    private void ChangeTraining(string trainingId)
+    {
+        if (_targetFighter == null)
+        {
+            Debug.LogError("선수가 없습니다");
+            return;
+        }
+
+        _targetFighter.CurrentTrainingId = trainingId;
+        Debug.Log($"{_targetFighter.Name} 훈련 정책 변경 -> {trainingId}");
+        RefreshUI();
     }
 
     private void OnClick_Rest()
     {
-
+        ChangeTraining("training_00");
     }
 
     private void OnClick_HpTraining()
     {
-
+        ChangeTraining("training_01");
     }
     private void OnClick_AtkTraining()
     {
-
+        ChangeTraining("training_02");
     }
 
     private void OnClick_DefTraining()
     {
-
+        ChangeTraining("training_03");
     }
 
     private void OnClick_Close()
