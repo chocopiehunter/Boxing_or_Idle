734ff21f4696082fcab765f21ed09882549a114f
jaeho <>
2026-08-10 18:53:35 +0900

    [feat] TrainingManagementUI.cs 틀 작성 / UI 작업

--- Assets/Scripts/UI/TrainingManagementUI.cs
+++ Assets/Scripts/UI/TrainingManagementUI.cs
@@ -1,6 +1,51 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class TrainingManagementUI : UIBase
 {
-    
+    [SerializeField] private Text Text_Name;
+    [SerializeField] private Text Text_CurrentTraining;
+    [SerializeField] private Text Text_Stats;
+
+    [SerializeField] private UIButton Button_Rest;
+    [SerializeField] private UIButton Button_HpTraining;
+    [SerializeField] private UIButton Button_AtkTraining;
+    [SerializeField] private UIButton Button_DefTraining;
+    [SerializeField] private UIButton Button_Close;
+
+    private FighterModel _targetFighter;
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void RefreshUI()
+    {
+
+    }
+
+    private void OnClick_Rest()
+    {
+
+    }
+
+    private void OnClick_HpTraining()
+    {
+
+    }
+    private void OnClick_AtkTraining()
+    {
+
+    }
+
+    private void OnClick_DefTraining()
+    {
+
+    }
+
+    private void OnClick_Close()
+    {
+        UIManager.Instance.ClosePopupUI(UIType.TrainingManagementUI);
+    }
 }
