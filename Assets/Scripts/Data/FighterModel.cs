2159274e82a2a070831911fbff5a188f1204b8ee
jaeho <>
2026-08-10 11:45:35 +0900

    [feat] FighterModel 작성

--- Assets/Scripts/Data/FighterModel.cs
+++ Assets/Scripts/Data/FighterModel.cs
@@ -1,6 +1,24 @@
 using UnityEngine;
 
-public class FighterRuntime
+public class FighterModel
 {
-    
+    public string DataId { get; private set; }
+    public string Name { get; private set; }
+    public float Hp { get; set; }
+    public float Atk { get; set; }
+    public float Def { get; set; }
+    public float Condition { get; set; }
+
+    public string CurrentTrainingId { get; set; }
+
+    public FighterModel(FighterData data, string defaultTrainingId)
+    {
+        DataId = data.Id;
+        Name = data.Name;
+        Hp = data.Hp;
+        Atk = data.Atk;
+        Def = data.Def;
+        Condition = 100f;
+        CurrentTrainingId = defaultTrainingId;
+    }
 }
