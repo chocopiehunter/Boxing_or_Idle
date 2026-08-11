201c909511340e6645a3269bb1d0c88c52baea9b
jaeho <>
2026-08-11 22:37:04 +0900

    [feat] MatchScheduleUI 생성 및 UIType 추가

---
+++ Assets/Scripts/UI/MatchScheduleUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class MatchScheduleUI : UIBase
+{
+    private void OnEnable()
+    {
+        
+    }
+
+    private void RefreshUI()
+    {
+
+    }
+
+    private void OnClick_Confirm()
+    {
+
+    }
+
+    private void OnClick_Close()
+    {
+
+    }
+}
