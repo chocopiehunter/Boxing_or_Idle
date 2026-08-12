65985cd562cd16dc955919a24df7098e3c5aed88
jaeho <>
2026-08-12 13:59:38 +0900

    [feat] MatchOpponentRowUI 생성 및 틀 작성

---
+++ Assets/Scripts/UI/MatchOpponentRowUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MatchOpponentRowUI : MonoBehaviour
+{
+    [SerializeField] private UIButton Button_Row;
+    [SerializeField] private Text Text_Rank;
+    [SerializeField] private Text Text_Name;
+    [SerializeField] private Text Text_Stats;
+
+    private void OnClick_Row()
+    {
+
+    }
+}
