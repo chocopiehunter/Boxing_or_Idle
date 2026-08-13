a11f767c3fa86fdd99da4e0b030bac38aecd560a
jaeho <>
2026-08-13 16:28:04 +0900

    [feat] 라운드 승수 집계 및 KO 즉시 종료

--- Assets/Scripts/Manager/MatchManager.cs
+++ Assets/Scripts/Manager/MatchManager.cs
@@ -70,9 +70,45 @@ public class MatchManager : MonoBehaviour
             // KO시 즉시 경기 종료 로직
             if (playerRemainingHp <= 0f && opponentRemainingHp <= 0f)
             {
-
+                LastResult = MatchResult.Draw;
+                CurrentState = MatchState.Finished;
+                Debug.Log("동시 KO. 경기 종료 무승부");
+                return true;
             }
+
+            if(playerRemainingHp <= 0f)
+            {
+                LastResult = MatchResult.Lose;
+                CurrentState = MatchState.Finished;
+                Debug.Log($"{PlayerFighter.Name} {round}라운드 KO 패배");
+                return true;
+            }
+
+            if(opponentRemainingHp <= 0f)
+            {
+                LastResult = MatchResult.Win;
+                CurrentState = MatchState.Finished;
+                Debug.Log($"{PlayerFighter.Name} {round}라운드 KO 승리");
+                return true;
+            }
+
+            MatchResult roundResult = JudgeRoundByLostHpRate(playerLostRate, opponentLostRate);
+            if (roundResult == MatchResult.Win)
+            {
+                playerRoundWins = playerRoundWins + 1;
+            }
+            else
+            {
+                opponentRoundWins = opponentRoundWins + 1;
+            }
+
+            Debug.Log($"{round}라운드 판정 : {roundResult}");
         }
+
+        LastResult = JudgeMatchByRoundWins(playerRoundWins, opponentRoundWins);
+        CurrentState = MatchState.Finished;
+
+        Debug.Log($"경기 종료 {playerRoundWins} 대 {opponentRoundWins} {LastResult}");
 
         return true;
     }
