b6ea26fee4b53c095228badbc120442e7bebb0ef
jaeho <>
2026-08-16 18:15:51 +0900

    [add] 체육관 시스템 기초 스크립트 생성

---
+++ Assets/Scripts/Manager/GymManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class GymManager : MonoBehaviour
+{
+    public static GymManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        
+    }
+
+
+}
