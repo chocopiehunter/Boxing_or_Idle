4ec96e86ef5de85ffb0a15e15ce8b408f70a728c
jaeho <>
2026-08-16 20:57:51 +0900

    [feat] 시작 체육관 생성 로직과 ClearGym

--- Assets/Scripts/Manager/GymManager.cs
+++ Assets/Scripts/Manager/GymManager.cs
@@ -4,10 +4,39 @@ public class GymManager : MonoBehaviour
 {
     public static GymManager Instance { get; private set; }
 
+    [SerializeField] private int StartingLevel = 1;
+    [SerializeField] private int StartingGold = 300;
+
+    public GymModel CurrentGym {  get; private set; }
+
     private void Awake()
     {
-        
+        Instance = this;
     }
 
+    public void CreateStartingGym()
+    {
+        CurrentGym = new GymModel(StartingLevel, StartingGold);
+        Debug.Log($"시작 체육관 생성완료. 레벨: {CurrentGym.Level} / 자금: {CurrentGym.Gold}");
+    }
 
+    public void ClearGym()
+    {
+        CurrentGym = null;
+    }
+
+    //public GymLevelData GetCurrentLevelData()
+    //{
+
+    //}
+
+    //public GymLevelData GetNextLevelData()
+    //{
+
+    //}
+
+    //public bool TryUpgradeGym()
+    //{
+
+    //}
 }
