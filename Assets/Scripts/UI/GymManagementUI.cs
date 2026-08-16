1d4538d3e4f37d97fa90ce63f9e276872b73da4b
jaeho <>
2026-08-16 22:45:55 +0900

    [add] GymManagementUI 프리팹 생성 및 스크립트 기초 작성

---
+++ Assets/Scripts/UI/GymManagementUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GymManagementUI : UIBase
+{
+    [SerializeField] private UIButton Button_Close;
+    [SerializeField] private UIButton Button_Dim;
+    [SerializeField] private UIButton Button_Upgrade_Gym;
+    [SerializeField] private Text Text_UpgradeName;
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void RefreshUI()
+    {
+
+    }
+
+    private void OnClick_UpgradeGym()
+    {
+
+    }
+
+    private void OnClick_Close()
+    {
+
+    }
+}
