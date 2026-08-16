6733f6b4bcfe8c2c35f46228c8e2b7cc6b6b6316
jaeho <>
2026-08-16 18:39:04 +0900

    [feat] GymModel 기초 작성

--- Assets/Scripts/Data/GymModel.cs
+++ Assets/Scripts/Data/GymModel.cs
@@ -2,5 +2,24 @@
 
 public class GymModel
 {
+    public int Level { get; private set; }
+    public int Gold { get; private set; }
+
+    public GymModel (int startLevel, int startGold)
+    {
+
+    }
+
+    public void SetLevel()
+    {
+
+    }
+
+    public void AddGold()
+    {
+
+
+    }
+
     
 }
