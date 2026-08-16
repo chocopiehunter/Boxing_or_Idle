d07bc0e347d0fa02424084124fdf9f8f17063ed8
jaeho <>
2026-08-16 21:21:52 +0900

    [feat] GymManager 레벨 데이터를 Id 조회로 변경

--- Assets/Scripts/Manager/GymManager.cs
+++ Assets/Scripts/Manager/GymManager.cs
@@ -4,7 +4,7 @@ public class GymManager : MonoBehaviour
 {
     public static GymManager Instance { get; private set; }
 
-    [SerializeField] private int StartingLevel = 1;
+    [SerializeField] private string StartingLevelId = "gym_level_01";
     [SerializeField] private int StartingGold = 300;
 
     public GymModel CurrentGym {  get; private set; }
@@ -16,7 +16,14 @@ public class GymManager : MonoBehaviour
 
     public void CreateStartingGym()
     {
-        CurrentGym = new GymModel(StartingLevel, StartingGold);
+        GymLevelData startData = GameDataManager.Instance.GetGymLevelData(StartingLevelId);
+        if (startData == null)
+        {
+            Debug.LogError($"시작 체육관 데이터 없음. {StartingLevelId}");
+            return;
+        }
+
+        CurrentGym = new GymModel(startData, StartingGold);
         Debug.Log($"시작 체육관 생성완료. 레벨: {CurrentGym.Level} / 자금: {CurrentGym.Gold}");
     }
 
@@ -25,15 +32,31 @@ public class GymManager : MonoBehaviour
         CurrentGym = null;
     }
 
-    //public GymLevelData GetCurrentLevelData()
-    //{
+    public GymLevelData GetCurrentLevelData()
+    {
+        if (CurrentGym == null)
+        {
+            return null;
+        }
 
-    //}
+        return GameDataManager.Instance.GetGymLevelData(CurrentGym.LevelId);
+    }
 
-    //public GymLevelData GetNextLevelData()
-    //{
+    public GymLevelData GetNextLevelData()
+    {
+        GymLevelData currentData = GetCurrentLevelData();
+        if(currentData == null)
+        {
+            return null;
+        }
 
-    //}
+        if (string.IsNullOrEmpty(currentData.NextLevelId) == true)
+        {
+            return null;
+        }
+
+        return GameDataManager.Instance.GetGymLevelData(currentData.NextLevelId);
+    }
 
     //public bool TryUpgradeGym()
     //{
