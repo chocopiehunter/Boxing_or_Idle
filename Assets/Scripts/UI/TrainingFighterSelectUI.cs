69a5c7142ebe2ea73af54090c4f8f836d3b1f380
jaeho <>
2026-08-18 19:06:01 +0900

    [feat] SelectUI 중간저장

--- Assets/Scripts/UI/TrainingFighterSelectUI.cs
+++ Assets/Scripts/UI/TrainingFighterSelectUI.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class TrainingFighterSelectUI : UIBase
@@ -16,20 +17,40 @@ public class TrainingFighterSelectUI : UIBase
 
     private void OnEnable()
     {
-        
+        Button_Close.onClick.AddListener(OnClick_Close);
+        Button_Fighter1.onClick.AddListener(OnClick_Fighter1);
+        Button_Fighter2.onClick.AddListener(OnClick_Fighter2);
+        Button_Fighter3.onClick.AddListener(OnClick_Fighter3);
+        Button_Fighter4.onClick.AddListener(OnClick_Fighter4);
+
+        RefreshUI();
     }
 
     private void OnDisable()
     {
-        
+        Button_Close.onClick.RemoveListener(OnClick_Close);
+        Button_Fighter1.onClick.RemoveListener(OnClick_Fighter1);
+        Button_Fighter2.onClick.RemoveListener(OnClick_Fighter2);
+        Button_Fighter3.onClick.RemoveListener(OnClick_Fighter3);
+        Button_Fighter4.onClick.RemoveListener(OnClick_Fighter4);
     }
 
     private void RefreshUI()
     {
+        List<FighterModel> fighters = FighterManager.Instance.PlayerFighters;
+
+        RefreshSlot(Button_Fighter1, Text_Name_Fighter1, 0, fighters);
+        RefreshSlot(Button_Fighter2, Text_Name_Fighter2, 1, fighters);
+        RefreshSlot(Button_Fighter3, Text_Name_Fighter3, 2, fighters);
+        RefreshSlot(Button_Fighter4, Text_Name_Fighter4, 3, fighters);
+    }
+
+    private void RefreshSlot(Button button, Text nameText, int index, List<FighterModel> fighters)
+    {
 
     }
 
-    private void RefreshSlot()
+    private void OpenTrainingManagement(int index)
     {
 
     }
