8dafef7afd6a3b2919ed1f22e58b4b9ff1f20de1
jaeho <>
2026-08-20 15:38:00 +0900

    [add] 훈련시설용 ITrainingSpot, TrainingSpot 생성 및 작성

---
+++ Assets/Scripts/Common/TrainingSpot.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class TrainingSpot : MonoBehaviour, ITrainingSpot
+{
+    [SerializeField] private string TrainingDataIdValue;
+    [SerializeField] private bool IsUnlockedValue = true;
+    [SerializeField] private float BaseAttraction = 10f;
+    [SerializeField] private Transform TargetSpot;
+
+    public string TrainingDataId
+    {
+        get { return TrainingDataIdValue; }
+    }
+
+    public bool IsUnlocked
+    {
+        get { return IsUnlockedValue; }
+    }
+
+    public float GetAttractionScore(FighterModel fighter)
+    {
+        if (IsUnlocked == false)
+        {
+            return float.MinValue;
+        }
+
+        return BaseAttraction;
+    }
+
+    public Transform GetTargetSpot()
+    {
+        if (TargetSpot != null)
+        {
+            return TargetSpot;
+        }
+
+        return transform;
+    }
+}
