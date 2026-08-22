6a4733171c232b4e377ca9290920f02302754524
jaeho <>
2026-08-23 03:19:30 +0900

    [add] 시설 목록 보여줄 GymFacilityRowUI.cs 생성

---
+++ Assets/Scripts/UI/GymFacilityRowUI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GymFacilityRowUI : MonoBehaviour
+{
+    [SerializeField] private UIButton Button_Action;
+    [SerializeField] private Text Text_Level;
+    [SerializeField] private Text Text_Name;
+    [SerializeField] private Text Text_State;
+
+    private string _facilityType;
+    private Action<string> _onClick;
+
+    public void Setup()
+    {
+
+    }
+
+    private void OnClick_Action()
+    {
+
+    }
+}
