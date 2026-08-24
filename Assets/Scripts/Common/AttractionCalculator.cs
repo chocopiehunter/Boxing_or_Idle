a7f0113d0f27223c8f11182dfa73a1677a20b122
jaeho <>
2026-08-24 14:11:36 +0900

    [add] AttractionCalculator 생성 및 틀 작성

---
+++ Assets/Scripts/Common/AttractionCalculator.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public static class AttractionCalculator
+{
+    private const string RestTrainingType = "Rest";
+    private const float RestTrainingStaminaMin = 0f;
+
+    public static float Caculate(float baseAttraction, float restMinBonus, string trainingDataId, FighterModel fighter)
+    {
+        float attractionScore = baseAttraction;
+
+        if (fighter == null)
+        {
+            return attractionScore;
+        }
+
+        if (GameDataManager.Instance == null)
+        {
+            return attractionScore;
+        }
+
+        TrainingData trainingData = GameDataManager.Instance.GetTrainingData(trainingDataId);
+
+        if (trainingData == null)
+        {
+            return attractionScore;
+        }
+
+        return Mathf.Max(0f, attractionScore);
+    }
+
+    private static float AddPolicyAttractionBonus(float attractionScore, TrainingData trainingData, FighterModel fighter)
+    {
+        return attractionScore;
+    }
+
+    private static float AddRestAttractionBonus(float attractionScore, float restMinBonus, TrainingData trainingData, FighterModel fighter)
+    {
+        return attractionScore + restMinBonus;
+    }
+}
