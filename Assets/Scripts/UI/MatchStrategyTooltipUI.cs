ce09adf3eb874f1d8ee8558abb220f98c96ddd09
jaeho <>
2026-08-26 18:25:58 +0900

    [add] TooltipUI 생성 및 설명 표시 구현

---
+++ Assets/Scripts/UI/MatchStrategyTooltipUI.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MatchStrategyTooltipUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private MatchStrategySelectionUI _owner;
+    private string _description;
+
+    public void Setup(MatchStrategySelectionUI owner, string description)
+    {
+        _owner = owner;
+        _description = description;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (_owner == null)
+        {
+            return;
+        }
+
+        _owner.ShowDescription(_description);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (_owner == null)
+        {
+            return;
+        }
+
+        _owner.HideDescription();
+    }
+}
