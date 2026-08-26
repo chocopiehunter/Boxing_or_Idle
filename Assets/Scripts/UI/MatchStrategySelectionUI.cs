4e61787203fb08c28912d3339167442182a63aad
jaeho <>
2026-08-26 20:07:35 +0900

    [feat] SelectionUI 상위 전략 선택지 표시 구현

--- Assets/Scripts/UI/MatchStrategySelectionUI.cs
+++ Assets/Scripts/UI/MatchStrategySelectionUI.cs
@@ -6,6 +6,23 @@ public class MatchStrategySelectionUI : UIBase
 {
     [SerializeField] private Text Text_StrategyDescription;
 
+    [SerializeField] private UIButton Button_Strategy1;
+    [SerializeField] private UIButton Button_Strategy2;
+    [SerializeField] private UIButton Button_Strategy3;
+    [SerializeField] private UIButton Button_Strategy4;
+
+    [SerializeField] private Text Text_StrategyName1;
+    [SerializeField] private Text Text_StrategyName2;
+    [SerializeField] private Text Text_StrategyName3;
+    [SerializeField] private Text Text_StrategyName4;
+
+    [SerializeField] private MatchStrategyTooltipUI Tooltip_Strategy1;
+    [SerializeField] private MatchStrategyTooltipUI Tooltip_Strategy2;
+    [SerializeField] private MatchStrategyTooltipUI Tooltip_Strategy3;
+    [SerializeField] private MatchStrategyTooltipUI Tooltip_Strategy4;
+
+    private List<MatchStrategyOptionData> _visibleStrategyOptions = new List<MatchStrategyOptionData>();
+
     private void Awake()
     {
         if (Text_StrategyDescription != null)
@@ -14,6 +31,11 @@ public class MatchStrategySelectionUI : UIBase
         }
 
         HideDescription();
+    }
+
+    private void OnEnable()
+    {
+        RefreshRootStrategyOptions();
     }
 
     public void ShowDescription(string description)
@@ -36,5 +58,57 @@ public class MatchStrategySelectionUI : UIBase
 
         Text_StrategyDescription.text = "";
         Text_StrategyDescription.gameObject.SetActive(false);
+    }
+
+    private void RefreshRootStrategyOptions()
+    {
+        HideDescription();
+
+        if (GameDataManager.Instance == null)
+        {
+            Debug.LogError("경기 전략 선택지 표시 실패 GameDataManager 없음");
+            return;
+        }
+
+        _visibleStrategyOptions = GameDataManager.Instance.GetRootMatchStrategyOptions();
+
+        RefreshStrategySlot(Button_Strategy1, Text_StrategyName1, Tooltip_Strategy1, 0);
+        RefreshStrategySlot(Button_Strategy2, Text_StrategyName2, Tooltip_Strategy2, 1);
+        RefreshStrategySlot(Button_Strategy3, Text_StrategyName3, Tooltip_Strategy3, 2);
+        RefreshStrategySlot(Button_Strategy4, Text_StrategyName4, Tooltip_Strategy4, 3);
+    }
+
+    private void RefreshStrategySlot(UIButton button, Text nameText, MatchStrategyTooltipUI tooltip, int index)
+    {
+        if (index < 0 || index >= _visibleStrategyOptions.Count)
+        {
+            button.SetInteractable(false);
+            nameText.text = "미등록";
+            tooltip.Setup(this, "");
+            return;
+        }
+
+        MatchStrategyOptionData optionData = _visibleStrategyOptions[index];
+
+        if (optionData == null)
+        {
+            button.SetInteractable(false);
+            nameText.text = "미등록";
+            tooltip.Setup(this, "");
+            return;
+        }
+
+        nameText.text = optionData.Name;
+
+        tooltip.Setup(this, optionData.Description);
+
+        bool interactable = true;
+
+        if (optionData.ActionType == MatchStrategyOptionActionType.Disabled)
+        {
+            interactable = false;
+        }
+
+        button.SetInteractable(interactable);
     }
 }
