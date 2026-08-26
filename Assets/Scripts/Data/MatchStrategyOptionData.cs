8c08f71287870500b56345cd95a1810b2d3ebc67
jaeho <>
2026-08-26 17:16:48 +0900

    [add] 전략 옵션 데이터 추가

---
+++ Assets/Scripts/Data/MatchStrategyOptionData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class MatchStrategyOptionData : GameDataBase
+{
+    public string Name;
+    public string Description;
+    public string ParentOptionId;
+    public string ActionType;
+    public string StrategyId;
+    public int SortOrder;
+}
+
+public static class MatchStrategyOptionActionType
+{
+    public const string OpenSubOptions = "OpenSubOptions";
+    public const string ApplyStrategy = "ApplyStrategy";
+    public const string KeepCurrent = "KeepCurrent";
+    public const string Disabled = "Disabled";
+}
