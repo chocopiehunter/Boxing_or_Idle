2788460ff435430ee86548d9b3f5f0c17e63e92e
jaeho <>
2026-08-26 18:14:34 +0900

    [add] 전략선택UI 생성 및 마우스 툴팁 기초 작성

---
+++ Assets/Scripts/UI/MatchStrategySelectionUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MatchStrategySelectionUI : UIBase
+{
+    [SerializeField] private Text Text_StrategyDescription;
+
+    private void Awake()
+    {
+        if (Text_StrategyDescription != null)
+        {
+            Text_StrategyDescription.raycastTarget = false;
+        }
+
+        HideDescription();
+    }
+
+    public void ShowDescription(string description)
+    {
+        if (Text_StrategyDescription == null)
+        {
+            return;
+        }
+
+        Text_StrategyDescription.text = description;
+        Text_StrategyDescription.gameObject.SetActive(true);
+    }
+
+    public void HideDescription()
+    {
+        if (Text_StrategyDescription == null)
+        {
+            return;
+        }
+
+        Text_StrategyDescription.text = "";
+        Text_StrategyDescription.gameObject.SetActive(false);
+    }
+}
