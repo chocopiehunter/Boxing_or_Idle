e3cca14cadcef36e9e236a3381b8decbb82b3f02
jaeho <>
2026-08-27 17:39:45 +0900

    [feat] TransitionLoadingUI 최소 연출 시간 대기 로직

--- Assets/Scripts/UI/TransitionLoadingUI.cs
+++ Assets/Scripts/UI/TransitionLoadingUI.cs
@@ -1,7 +1,26 @@
-﻿using UnityEngine;
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class TransitionLoadingUI : UIBase
 {
+    [SerializeField] private float MinimumDisplaySeconds = 2f;
 
+    public async UniTask WaitForSeconds()
+    {
+        if (MinimumDisplaySeconds <= 0f)
+        {
+            return;
+        }
+
+        CancellationToken cancellationToken = this.GetCancellationTokenOnDestroy();
+        float passedSeconds = 0f;
+
+        while (passedSeconds < MinimumDisplaySeconds)
+        {
+            await UniTask.Yield(PlayerLoopTiming.Update, cancellationToken);
+            passedSeconds = passedSeconds + Time.unscaledDeltaTime;
+        }
+    }
 }
