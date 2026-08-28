855de92d4a8c4406bef6f962bb05ea14f4534d52
jaeho <>
2026-08-28 11:14:04 +0900

    [add] MatchCombatModel 생성 및 틀 작성

---
+++ Assets/Scripts/Fighting/MatchCombatModel.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class MatchCombatModel
+{
+    public MatchSituation CurrentSituation { get; private set; }
+    public WrestlingSituation CurrentWrestlingSituation { get; private set; }
+    public GroundPosition CurrentGroundPosition { get; private set; }
+
+    public MatchFighterSide Attacker { get; private set; }
+    public MatchFighterSide Defender { get; private set; }
+
+    public MatchCombatModel()
+    {
+        Reset();
+    }
+
+    public void StartRound()
+    {
+        ChangeToStanding();
+    }
+
+    public void ChangeToStanding()
+    {
+
+    }
+
+    public void ChangeToWrestling()
+    {
+
+    }
+
+    public void ChangeToGround()
+    {
+
+    }
+
+    public void Reset()
+    {
+        CurrentSituation = MatchSituation.None;
+        CurrentWrestlingSituation = WrestlingSituation.None;
+        CurrentGroundPosition = GroundPosition.None;
+    }
+}
