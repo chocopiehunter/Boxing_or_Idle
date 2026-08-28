1779a56db12b21db0654d6a88d73923de982c9a3
jaeho <>
2026-08-28 18:34:06 +0900

    [add] MatchFighterModel 생성 및 중간 저장

---
+++ Assets/Scripts/Fighting/MatchFighterModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchFighterModel
+{
+    public MatchFighterSide FighterSide { get; private set; }
+
+    private List<string> _ownedSkillIds;
+
+    private MatchCoolTimeModel _coolTimeModel;
+
+    public IReadOnlyList<string> OwnedSkillIds
+    {
+        get
+        {
+            return _ownedSkillIds;
+        }
+    }
+
+    public MatchFighterModel(MatchFighterSide fighterSide, IReadOnlyList<string> ownedSkillIds)
+    {
+
+
+    }
+
+    public bool HasSkill(string skillId)
+    {
+        if (string.IsNullOrEmpty(skillId))
+        {
+            return false;
+        }
+
+        return _ownedSkillIds.Contains(skillId);
+    }
+
+    public bool IsSkillReady(string skillId)
+    {
+        if (HasSkill(skillId) == false)
+        {
+            return false;
+        }
+
+        return _coolTimeModel.IsSkillReady(skillId);
+    }
+}
