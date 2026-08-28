d3a85cba6b3e9458a007d52464ddce873d6f0e29
jaeho <>
2026-08-28 16:07:17 +0900

    [add] 스킬 쿨타임 관리용 MatchCoolTimeModel 생성

---
+++ Assets/Scripts/Fighting/MatchCoolTimeModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchCoolTimeModel
+{
+    private Dictionary<string, float> _remainingSecondsBySkillId;
+    private List<string> _coolingSkillIds;
+
+    public void Reset()
+    {
+        _remainingSecondsBySkillId.Clear();
+        _coolingSkillIds.Clear();
+    }
+}
