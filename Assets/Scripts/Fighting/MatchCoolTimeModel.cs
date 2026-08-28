529eaa99da8b14f5a810d638307f8c615055a87a
jaeho <>
2026-08-28 16:19:17 +0900

    [feat] 쿨타임 중간저장

--- Assets/Scripts/Fighting/MatchCoolTimeModel.cs
+++ Assets/Scripts/Fighting/MatchCoolTimeModel.cs
@@ -6,6 +6,40 @@ public class MatchCoolTimeModel
     private Dictionary<string, float> _remainingSecondsBySkillId;
     private List<string> _coolingSkillIds;
 
+    public MatchCoolTimeModel()
+    {
+        _remainingSecondsBySkillId = new Dictionary<string, float>();
+        _coolingSkillIds = new List<string>();
+    }
+
+    public bool IsSkillReady(string skillId)
+    {
+        if (string.IsNullOrEmpty(skillId))
+        {
+            return false;
+        }
+
+        return _remainingSecondsBySkillId.ContainsKey(skillId) == false;
+    }
+
+    public bool TryStartSkillCooldown(SkillData skillData)
+    {
+
+
+        return true;
+    }
+
+    public void UpdateCooldown(float passedSeconds)
+    {
+
+    }
+
+    public float GetRemainingSeconds(string skillId)
+    {
+
+        return _remainingSecondsBySkillId[skillId];
+    }
+
     public void Reset()
     {
         _remainingSecondsBySkillId.Clear();
