0a23b9c9be79ba37d37e5de417cc83cf524642ff
jaeho <>
2026-08-28 15:05:24 +0900

    [feat] 전투 행동 간격 관리 MatchCombatRunner 생성 및 작성

---
+++ Assets/Scripts/Fighting/MatchCombatRunner.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class MatchCombatRunner
+{
+    private readonly MatchCombatModel _combatModel;
+    private readonly float _actionIntervalSeconds;
+    private float _actionPassedSeconds;
+
+    public MatchCombatRunner(MatchCombatModel combatModel, float actionIntervalSeconds)
+    {
+        _combatModel = combatModel;
+        _actionIntervalSeconds = actionIntervalSeconds;
+
+        Reset();
+    }
+
+    public bool UpdateActionTime(float passedSeconds)
+    {
+        if (passedSeconds <= 0f)
+        {
+            return false;
+        }
+
+        if (_combatModel == null)
+        {
+            return false;
+        }
+
+        if (_combatModel.CurrentSituation == MatchSituation.None)
+        {
+            return false;
+        }
+
+        if (_actionIntervalSeconds <= 0f)
+        {
+            return false;
+        }
+
+        _actionPassedSeconds = _actionPassedSeconds + passedSeconds;
+        
+        if (_actionPassedSeconds < _actionIntervalSeconds)
+        {
+            return false;
+        }
+
+        _actionPassedSeconds = _actionPassedSeconds - _actionIntervalSeconds;
+
+        return true;
+    }
+
+    public void Reset()
+    {
+        _actionPassedSeconds = 0f;
+    }
+}
