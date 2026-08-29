2b3c2f92af5109fd665a42e54d882af76a02d2ea
jaeho <>
2026-08-29 20:06:35 +0900

    [feat] 스킬 사용 상황 및 조건 판정 로직 구현

--- Assets/Scripts/Fighting/SkillUseConditionChecker.cs
+++ Assets/Scripts/Fighting/SkillUseConditionChecker.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SkillUseConditionChecker
@@ -25,6 +26,98 @@ public class SkillUseConditionChecker
             return false;
         }
 
+        for (int index = 0; index < conditionDataList.Count; index++)
+        {
+            SkillUseConditionData conditionData = conditionDataList[index];
+
+            if (IsConditionMet(conditionData, combatModel, skillUserSide))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private bool IsConditionMet(SkillUseConditionData conditionData, MatchCombatModel combatModel, MatchFighterSide skillUserSide)
+    {
+        if (conditionData == null)
+        {
+            return false;
+        }
+
+        MatchSituation requiredMatchSituation;
+
+        if (Enum.TryParse(conditionData.MatchSituation, out requiredMatchSituation) == false)
+        {
+            return false;
+        }
+
+        if (combatModel.CurrentSituation != requiredMatchSituation)
+        {
+            return false;
+        }
+
+        WrestlingSituation requiredWrestlingSituation;
+
+        if (Enum.TryParse(conditionData.WrestlingSituation, out requiredWrestlingSituation) == false)
+        {
+            return false;
+        }
+
+        if (combatModel.CurrentWrestlingSituation != requiredWrestlingSituation)
+        {
+            return false;
+        }
+
+        GroundPosition requiredGroundPosition;
+
+        if (Enum.TryParse(conditionData.GroundPosition, out requiredGroundPosition) == false)
+        {
+            return false;
+        }
+
+        if (combatModel.CurrentGroundPosition != requiredGroundPosition)
+        {
+            return false;
+        }
+
+        SkillUserRole requiredUserRole;
+
+        if (Enum.TryParse(conditionData.UserRole, out requiredUserRole) == false)
+        {
+            return false;
+        }
+
+        return IsUserRoleMet(requiredUserRole, combatModel, skillUserSide);
+    }
+
+    private bool IsUserRoleMet(SkillUserRole requiredUserRole, MatchCombatModel combatModel, MatchFighterSide skillUserSide)
+    {
+        if (requiredUserRole == SkillUserRole.Any)
+        {
+            return true;
+        }
+
+        if (requiredUserRole == SkillUserRole.Attacker)
+        {
+            return combatModel.Attacker == skillUserSide;
+        }
+
+        if (requiredUserRole == SkillUserRole.Defender)
+        {
+            return combatModel.Defender == skillUserSide;
+        }
+
+        if (requiredUserRole == SkillUserRole.Top)
+        {
+            return combatModel.TopSide == skillUserSide;
+        }
+
+        if (requiredUserRole == SkillUserRole.Bottom)
+        {
+            return combatModel.BottomSide == skillUserSide;
+        }
 
         return false;
     }
