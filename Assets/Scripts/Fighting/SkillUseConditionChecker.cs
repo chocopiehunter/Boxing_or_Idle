6604e6ba2716d26ba3426f30ce8f432de3e16f95
jaeho <>
2026-08-29 19:32:54 +0900

    [add] 스킬 사용 판정용 SkillUseConditionChecker 생성 및 중간저장

---
+++ Assets/Scripts/Fighting/SkillUseConditionChecker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillUseConditionChecker
+{
+    public bool CanUseSkill(List<SkillUseConditionData> conditionDataList, MatchCombatModel combatModel, MatchFighterSide skillUserSide)
+    {
+        if (conditionDataList == null)
+        {
+            return false;
+        }
+
+        if (conditionDataList.Count == 0)
+        {
+            return false;
+        }
+
+        if (combatModel == null)
+        {
+            return false;
+        }
+
+        if (skillUserSide == MatchFighterSide.None)
+        {
+            return false;
+        }
+
+
+        return false;
+    }
+}
