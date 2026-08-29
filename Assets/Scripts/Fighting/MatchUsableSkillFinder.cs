36170af59b1b8058ca7ce60ead044c1fb551926e
jaeho <>
2026-08-29 20:32:32 +0900

    [feat] 사용 가능한 스킬 목록 구성하는 로직 구현

---
+++ Assets/Scripts/Fighting/MatchUsableSkillFinder.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchUsableSkillFinder
+{
+    private GameDataManager _gameDataManager;
+    private SkillUseConditionChecker _conditionChecker;
+
+    public MatchUsableSkillFinder(GameDataManager gameDataManager)
+    {
+        _gameDataManager = gameDataManager;
+        _conditionChecker = new SkillUseConditionChecker();
+    }
+
+    public List<SkillData> GetUsableSkills(MatchFighterModel fighter, MatchCombatModel combatModel)
+    {
+        List<SkillData> usableSkills = new List<SkillData>();
+
+        if (_gameDataManager == null)
+        {
+            return usableSkills;
+        }
+
+        if (fighter == null)
+        {
+            return usableSkills;
+        }
+
+        if (combatModel == null)
+        {
+            return usableSkills;
+        }
+
+        IReadOnlyList<string> ownedSkillIds = fighter.OwnedSkillIds;
+
+        if (ownedSkillIds == null)
+        {
+            return usableSkills;
+        }
+
+        for (int index = 0; index < ownedSkillIds.Count; index++)
+        {
+            string skillId = ownedSkillIds[index];
+
+            if (fighter.IsSkillReady(skillId) == false)
+            {
+                continue;
+            }
+
+            SkillData skillData = _gameDataManager.GetSkillData(skillId);
+
+            if (skillData == null)
+            {
+                continue;
+            }
+
+            List<SkillUseConditionData> conditionDataList = _gameDataManager.GetSkillUseConditions(skillId);
+
+            bool canUseSkill = _conditionChecker.CanUseSkill(conditionDataList, combatModel, fighter.FighterSide);
+
+            if (canUseSkill == false)
+            {
+                continue;
+            }
+
+            usableSkills.Add(skillData);
+        }
+
+        return usableSkills;
+    }
+}
