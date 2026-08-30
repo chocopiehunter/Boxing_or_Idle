ad597bca0b38726a51bd412a1e59660754b429ac
jaeho <>
2026-08-30 21:58:30 +0900

    [feat] 타격 성공 판정 로직

---
+++ Assets/Scripts/Fighting/CombatActionResult.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CombatActionResult
+{
+    public MatchCombatAction Action { get; private set; }
+    public bool IsSuccess { get; private set; }
+    public float SuccessChance { get; private set; }
+
+    public CombatActionResult(MatchCombatAction action, bool isSuccess, float successChance)
+    {
+        Action = action;
+        IsSuccess = isSuccess;
+        SuccessChance = successChance;
+    }
+}
---
+++ Assets/Scripts/Fighting/StrikeCalculator.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class StrikeCalculator
+{
+    public bool TryCalculate(MatchCombatAction action, MatchFighterModel skillUser, MatchFighterModel target, out CombatActionResult actionResult)
+    {
+        actionResult = null;
+
+        if (action == null || action.SelectedSkill == null)
+        {
+            return false;
+        }
+
+        if (skillUser == null || target == null)
+        {
+            return false;
+        }
+
+        if (action.SkillUserSide != skillUser.FighterSide)
+        {
+            return false;
+        }
+
+        if (action.TargetSide != target.FighterSide)
+        {
+            return false;
+        }
+
+        if (action.SelectedSkill.Category != SkillCategoryType.Strike)
+        {
+            return false;
+        }
+
+        float successChance = CalculateSuccessChance(action.SelectedSkill, skillUser.StandingOffense, target.StandingDefense);
+
+        float selectedChance = Random.Range(0f, 100f);
+
+        bool isSuccess = selectedChance < successChance;
+
+        actionResult = new CombatActionResult(action, isSuccess, successChance);
+
+        return true;
+    }
+
+    private float CalculateSuccessChance(SkillData skillData, float offense, float defense)
+    {
+        float successChance = skillData.BaseSuccessChance;
+        float totalStat = offense + defense;
+
+        if (totalStat > 0f)
+        {
+            float offenseRate = offense / totalStat;
+
+            successChance = skillData.BaseSuccessChance * offenseRate * 2f;
+        }
+
+        successChance = Mathf.Clamp(successChance, 0f, 100f);
+
+        return successChance;
+    }
+}
