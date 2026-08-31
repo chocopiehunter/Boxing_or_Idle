2e9799d7fd3efad2f6f166020dedcad797fa22f1
jaeho <>
2026-08-31 16:29:09 +0900

    [feat] 클린치 상황 주도권 계산 로직

---
+++ Assets/Scripts/Fighting/ClinchCalculator.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class ClinchCalculator
+{
+    public bool TryCalculate(MatchCombatAction action, MatchFighterModel skillUser, MatchFighterModel target, out CombatActionResult actionResult)
+    {
+        actionResult = null;
+
+        if (action == null || action.SelectedSkill == null)
+        {
+            return false;
+        }
+
+        if (skillUser == null || target == null)
+        {
+            return false;
+        }
+
+        if (action.SkillUserSide != skillUser.FighterSide) 
+        {
+            return false;
+        }
+
+        if (action.TargetSide != target.FighterSide)
+        {
+            return false;
+        }
+
+        if (action.SelectedSkill.ActionType != SkillActionType.ClinchEntry)
+        {
+            return false;
+        }
+
+        float controlChance = SuccessChanceCalculator.Calculate(action.SelectedSkill.BaseSuccessChance, skillUser.WrestlingOffense, target.WrestlingDefense);
+        float selectedChance = Random.Range(0f, 100f);
+
+        bool skillUserHasControl = selectedChance < controlChance;
+
+        CombatActionResultType resultType = CombatActionResultType.ClinchReversed;
+
+        if (skillUserHasControl)
+        {
+            resultType = CombatActionResultType.ClinchStarted;
+        }
+
+        actionResult = new CombatActionResult(action, resultType, skillUserHasControl, controlChance, 0f);
+
+        return true;
+    }
+}
