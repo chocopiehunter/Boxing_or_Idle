603faf9756f2a025ffa3876cd4f1112092468a53
jaeho <>
2026-09-02 14:26:52 +0900

    [feat] 경기중 스태미너 기반 능력치 감소 계산기 추가

---
+++ Assets/Scripts/Fighting/StaminaPenaltyCalculator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public static class StaminaPenaltyCalculator
+{
+    private const float MinStatRate = 0.5f;
+
+    public static float ApplyStaminaPenalty(float baseStat, MatchFighterModel fighter)
+    {
+        if (baseStat <= 0f)
+        {
+            return 0f;
+        }
+
+        if (fighter == null)
+        {
+            return baseStat;
+        }
+
+        if (fighter.MaxStamina <= 0f)
+        {
+            return baseStat;
+        }
+
+        float staminaRate = fighter.CurrentStamina / fighter.MaxStamina;
+        staminaRate = Mathf.Clamp01(staminaRate);
+
+        float statRate = MinStatRate + ((1f - MinStatRate) * staminaRate);
+
+        return baseStat * statRate;
+    }
+}
