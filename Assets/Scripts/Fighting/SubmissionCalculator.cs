625e0a7144833c972c92523d7591dccf30e336fc
jaeho <>
2026-09-03 11:38:03 +0900

    [add] SubmissionCalculator 생성 및 중간저장

---
+++ Assets/Scripts/Fighting/SubmissionCalculator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class SubmissionCalculator
+{
+    private const float MinDamage = 1f;
+
+    public bool TryCalculate(MatchCombatAction action, MatchFighterModel skillUser, MatchFighterModel target, GroundPositionData currentPositionData, 
+                             float currentSubmissionResistHp, out CombatActionResult actionResult)
+    {
+        actionResult = null;
+
+        if (action == null || action.SelectedSkill == null)
+        {
+            return false;
+        }
+
+        if (skillUser == null || target == null || currentPositionData == null)
+        {
+            return false;
+        }
+
+        if (action.SkillUserSide != skillUser.FighterSide)
+        {
+            return false;
+        }
+
+        if (action.TargetSide != target.FighterSide)
+        {
+            return false;
+        }
+
+        if (action.SelectedSkill.ActionType != SkillActionType.Submission)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
