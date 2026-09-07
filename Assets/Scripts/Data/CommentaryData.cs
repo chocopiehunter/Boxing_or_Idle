a2746b25fa7697f937e9d0ee23c2ff0537aa5b18
jaeho <>
2026-09-07 14:01:16 +0900

    [add] CommentaryData 추가

---
+++ Assets/Scripts/Data/CommentaryData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class CommentaryData : GameDataBase
+{
+    public string ResultType;
+    public string SkillId;
+    public string MatchSituation;
+    public string WrestlingSituation;
+    public string GroundPosition;
+    public string SkillUserSide;
+    public string CommentaryText;
+    public int Weight;
+}
