55158aae92569f74cf650faf494fb1cb7b7b1350
jaeho <>
2026-09-07 10:46:38 +0900

    [feat] CommentaryRowUI 생성 및 틀 작성

---
+++ Assets/Scripts/UI/CommentaryRowUI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CommentaryRowUI : MonoBehaviour
+{
+    [SerializeField] private RectTransform RectTransform_Row;
+    [SerializeField] private CanvasGroup CanvasGroup_Row;
+    [SerializeField] private LayoutElement LayoutElement_Row;
+    [SerializeField] private Text Text_Commentary;
+
+    [SerializeField] private float MinHeight = 50f;
+    [SerializeField] private float VerticalPadding = 10f;
+
+    private float _targetY;
+    public float Height
+    {
+        get
+        {
+            return RectTransform_Row.rect.height;
+        }
+    }
+
+    public void Setup(string message)
+    {
+
+    }
+
+    public void UpdatePosition()
+    {
+
+    }
+
+    public void ResetRow()
+    {
+
+    }
+}
