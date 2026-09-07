d437436464a82bf40d6d1a378e19f96a180ba138
jaeho <>
2026-09-07 11:57:41 +0900

    [feat] CommentaryUI 생성 및 틀 작성

---
+++ Assets/Scripts/UI/CommentaryUI.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class CommentaryUI : MonoBehaviour
+{
+    [SerializeField] private RectTransform Transform_Content;
+    [SerializeField] private CommentaryRowUI Prefab_Row;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void ClearCommentary()
+    {
+
+    }
+
+    private void InitializePool()
+    {
+
+    }
+
+    private CommentaryRowUI CreateRow()
+    {
+        CommentaryRowUI row = Instantiate(Prefab_Row, Transform_Content);
+
+        return row;
+    }
+
+    private void TakeFromPool()
+    {
+
+    }
+
+    private void ReturnToPool()
+    {
+
+    }
+
+    private void DestroyRow()
+    {
+
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        
+    }
+}
