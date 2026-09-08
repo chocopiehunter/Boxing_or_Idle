4cf178e5fdc0a3d13db6e7fbfc74cfc8408e8c66
jaeho <>
2026-09-08 10:38:10 +0900

    [add] InventoryModel / InventoryEntry 생성 및 틀 작성

---
+++ Assets/Scripts/Data/InventoryModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryModel
+{
+    private readonly Dictionary<string, int> _itemCounts = new Dictionary<string, int>();
+
+    public int GetItemCount()
+    {
+        return 0;
+    }
+
+    public bool TryAddItem()
+    {
+        return true;
+    }
+
+    public bool TryRemoveItem()
+    {
+        return true;
+    }
+
+    public void Clear()
+    {
+        _itemCounts.Clear();
+    }
+}
