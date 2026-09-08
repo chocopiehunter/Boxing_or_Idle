221e419efc2146f5c2c91b3e4894c26daa758d73
jaeho <>
2026-09-08 11:11:01 +0900

    [feat] InventoryManager 중간저장

--- Assets/Scripts/Manager/InventoryManager.cs
+++ Assets/Scripts/Manager/InventoryManager.cs
@@ -1,6 +1,49 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
-    
+    public static InventoryManager Instance { get; private set; }
+
+    [SerializeField] private List<InventoryEntry> StartingItems = new List<InventoryEntry>();
+
+    public InventoryModel CurrentInventory { get; private set; } = new InventoryModel();
+
+    public event Action<string, int, int> OnItemCountChanged;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void CreateStartingInventory()
+    {
+
+    }
+
+    public int GetItemCount()
+    {
+        return 0;
+    }
+
+    public bool HasItem()
+    {
+        return true;
+    }
+
+    public List<InventoryEntry> GetAllEntries()
+    {
+        return CurrentInventory.GetAllEntries();
+    }
+
+    public bool TryAddItem()
+    {
+        return true;
+    }
+
+    public bool TryRemoveItem()
+    {
+        return true;
+    }
 }
