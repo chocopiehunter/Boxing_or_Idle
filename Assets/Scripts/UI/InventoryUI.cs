2122971ca4c0cfa192932b3fcf37885be4f5735b
jaeho <>
2026-09-08 13:34:05 +0900

    [add] InventoryUI 생성 및 기본 틀 작성

---
+++ Assets/Scripts/UI/InventoryUI.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class InventoryUI : UIBase
+{
+    [SerializeField] private UIButton Button_Consumable;
+    [SerializeField] private UIButton Button_KeyItem;
+    [SerializeField] private UIButton Button_Close;
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    private void SelectItemType()
+    {
+
+    }
+
+    private void RefreshItemListUI()
+    {
+
+    }
+
+    private void ClearItemRowUI()
+    {
+
+    }
+
+    private void OnClick_Comsumable()
+    {
+
+    }
+
+    private void OnClick_KeyItem()
+    {
+
+    }
+
+    private void OnClick_Close()
+    {
+        UIManager.Instance.CloseContentUI(UIType.InventoryUI);
+    }
+}
